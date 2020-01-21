2210ab708d5ad2ad6347a0ad5836d162708da301
Francisco Romero <francisco.romero@sjsu.edu>
2020-01-21 13:53:45 -0800

    Added drawer to student homepage

--- quickmaths/screens/StudentHomeScreen.js
+++ quickmaths/screens/StudentHomeScreen.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
+import Colors from '../constants/Colors';
+import {Item, HeaderButtons} from 'react-navigation-header-buttons';
+import HeaderButton from '../components/HeaderButton';
+
 const StudentHomeScreen = props => {
     return(
         <View style={styles.screen}>
@@ -16,5 +20,25 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     }
 });
+
+StudentHomeScreen.navigationOptions = (navData) => {
+    return{
+        headerTitle: 'Assignments',
+        headerLeftContainerStyle: {
+            backgroundColor: Colors.accentColor,
+        },
+        headerLeft: () => (
+               <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                    <Item 
+                        title="Menu" 
+                        iconName="md-menu" 
+                        onPress={() => {
+                            navData.navigation.toggleDrawer();
+                        }}
+                    />
+                </HeaderButtons>
+        )
+    };
+};
 
 export default StudentHomeScreen;
