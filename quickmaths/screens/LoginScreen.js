1309a620b7f15fdafc1f6bb0eb606499e2af234b
Mark <casapao.markkenneth@gmail.com>
2020-01-22 18:54:01 -0800

    Fixed an error in login screen

--- quickmaths/screens/LoginScreen.js
+++ quickmaths/screens/LoginScreen.js
@@ -1,83 +1,100 @@
-import React, { useState } from 'react';
-import {View, TextInput, Button, StyleSheet, Keyboard, TouchableWithoutFeedback} from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  Keyboard,
+  TouchableWithoutFeedback
+} from "react-native";
 
-import Background from '../components/Background';
-import AppTitle from '../constants/AppTitle';
-import StandardButton from '../components/StandardButton';
+import Background from "../components/Background";
+import AppTitle from "../constants/AppTitle";
+import StandardButton from "../components/StandardButton";
 
+//Blah blah blah
 const LoginScreen = props => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const onLogin = () => {
-        console.log(email);
-        console.log(password);
-        props.navigation.navigate('TeacherHomeScreen');
-    };
+  const onLogin = () => {
+    console.log(email);
+    console.log(password);
+    props.navigation.navigate("TeacherHomeScreen");
+  };
 
-    return(
-            <Background>
-                 <TouchableWithoutFeedback onPress={() => {Keyboard.dismiss()}}>
-                    <View style={styles.screen}>
-                        <View style={styles.titleContainer}>
-                            <AppTitle/>
-                        </View>
-                        <View style={styles.inputFieldContainer}>
-                            <TextInput
-                                style={styles.inputField}
-                                placeholder="Enter Email"
-                                placeholderTextColor='white'   
-                                onChangeText={(text) => setEmail(text)}
-                                value={email}
-                            />
-                        </View>
-                        <View style={styles.inputFieldContainer}>
-                            <TextInput
-                                style={styles.inputField}
-                                placeholder="Enter Password"
-                                placeholderTextColor='white'
-                                onChangeText={(text) => setPassword(text)}
-                                value={password}
-                            />
-                        </View>
-                        
-                        <StandardButton text="Login" onTap={onLogin}
-                        />
-                        <Button title="Forgot Password?" onPress={() => {
-                            props.navigation.navigate('EmailRecovery');
-                        }}/>
-                        <Button title="Not Registered?" onPress={() => {
-                            props.navigation.navigate('Register');
-                        }}/>
-                    </View>
-                </TouchableWithoutFeedback>
-            </Background>
-    );
+  return (
+    <Background>
+      <TouchableWithoutFeedback
+        onPress={() => {
+          Keyboard.dismiss();
+        }}
+      >
+        <View style={styles.screen}>
+          <View style={styles.titleContainer}>
+            <AppTitle />
+          </View>
+          <View style={styles.inputFieldContainer}>
+            <TextInput
+              style={styles.inputField}
+              placeholder="Enter Email"
+              placeholderTextColor="white"
+              onChangeText={text => setEmail(text)}
+              value={email}
+            />
+          </View>
+          <View style={styles.inputFieldContainer}>
+            <TextInput
+              style={styles.inputField}
+              placeholder="Enter Password"
+              placeholderTextColor="white"
+              onChangeText={text => setPassword(text)}
+              value={password}
+            />
+          </View>
+
+          <StandardButton text="Login" onTap={onLogin} />
+          <Button
+            title="Forgot Password?"
+            onPress={() => {
+              props.navigation.navigate("EmailRecovery");
+            }}
+          />
+          <Button
+            title="Not Registered?"
+            onPress={() => {
+              props.navigation.navigate("Register");
+            }}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+    </Background>
+  );
 };
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    titleContainer:{
-        paddingBottom: 20
-    },
-    inputFieldContainer: {
-        backgroundColor: 'rgba(0,0,0, 0.4)',
-        borderRadius: 20,
-        width: '75%',
-        height: 50,
-        flexDirection: "row",
-        alignItems: 'center',
-        justifyContent: 'space-around',
-        marginTop: 10
-    },
-    inputField: {
-        color: 'white',
-        fontSize: 20
-    }
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  titleContainer: {
+    paddingBottom: 20
+  },
+  inputFieldContainer: {
+    backgroundColor: "rgba(0,0,0, 0.4)",
+    borderRadius: 20,
+    width: "75%",
+    height: 50,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    marginTop: 10
+  },
+  inputField: {
+    color: "white",
+    fontSize: 20
+  }
 });
 
 export default LoginScreen;
--- quickmaths/screens/RegisterScreen.js
+++ quickmaths/screens/RegisterScreen.js
@@ -1,125 +1,140 @@
-import React, {useState} from 'react';
-import {View, TextInput, Button, StyleSheet, Keyboard, TouchableWithoutFeedback} from 'react-native';
-import {useDispatch} from 'react-redux';
+import React, { useState } from "react";
+import {
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  Keyboard,
+  TouchableWithoutFeedback
+} from "react-native";
+import { useDispatch } from "react-redux";
 
-import Background from '../components/Background';
-import AppTitle from '../constants/AppTitle';
-import StandardButton from '../components/StandardButton';
-import Checkbox from '../components/Checkbox';
-import {signInAsStudent, signInAsTeacher} from '../store/actions/users';
+import Background from "../components/Background";
+import AppTitle from "../constants/AppTitle";
+import StandardButton from "../components/StandardButton";
+import Checkbox from "../components/Checkbox";
+import { signInAsStudent, signInAsTeacher } from "../store/actions/users";
 
 const RegisterScreen = props => {
-    const [email, setEmail] = useState('');
-    const [name, setName] = useState('');
-    const [courseCode, setCourseCode] = useState('');
-    const [password, setPassword] = useState('');
-    const [selected, setSelected] = useState(false);
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [courseCode, setCourseCode] = useState("");
+  const [password, setPassword] = useState("");
+  const [selected, setSelected] = useState(false);
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const signInAsStudentHandler = () => {
-        dispatch(signInAsStudent());
-    };
+  const signInAsStudentHandler = () => {
+    dispatch(signInAsStudent());
+  };
 
-    const signInAsTeacherHandler = () => {
-        dispatch(signInAsTeacher());
-    };
+  const signInAsTeacherHandler = () => {
+    dispatch(signInAsTeacher());
+  };
 
-    const onRegister = () => {
-        console.log(email);
-        console.log(name);
-        console.log(courseCode);
-        console.log(password);
-        if(selected){
-            signInAsTeacherHandler();
-        }
-        else{
-            signInAsStudentHandler();
-        }
-        props.navigation.navigate(selected ? 'TeacherHomeScreen' : 'StudentHomeScreen');
+  const onRegister = () => {
+    console.log(email);
+    console.log(name);
+    console.log(courseCode);
+    console.log(password);
+    if (selected) {
+      signInAsTeacherHandler();
+    } else {
+      signInAsStudentHandler();
     }
-
-    const onSelect = () => {
-        setSelected(!selected);
-    }
-
-    return(
-        <Background>
-            <TouchableWithoutFeedback onPress={() => {Keyboard.dismiss()}}>
-                <View style={styles.screen}>
-                    <View style={styles.titleContainer}>
-                        <AppTitle/>
-                    </View>
-                    <View style={styles.inputFieldContainer}>
-                        <TextInput
-                            style={styles.inputField}
-                            placeholder="Enter Email"
-                            placeholderTextColor='white'   
-                            onChangeText={(text) => setEmail(text)}
-                            value={email}
-                        />
-                    </View>
-                    <View style={styles.inputFieldContainer}>
-                        <TextInput
-                            style={styles.inputField}
-                            placeholder="Enter Full Name"
-                            placeholderTextColor='white'   
-                            onChangeText={(text) => setName(text)}
-                            value={email}
-                        />
-                    </View>
-                    <View style={styles.inputFieldContainer}>
-                        <TextInput
-                            style={styles.inputField}
-                            placeholder="Enter Course Code"
-                            placeholderTextColor='white'   
-                            onChangeText={(text) => setCourseCode(text)}
-                            value={email}
-                        />
-                    </View>
-                    <View style={styles.inputFieldContainer}>
-                        <TextInput
-                            style={styles.inputField}
-                            placeholder="Enter Password"
-                            placeholderTextColor='white'
-                            onChangeText={(text) => setPassword(text)}
-                            value={password}
-                        />
-                    </View>
-                    <StandardButton text="Register" onTap={onRegister}/>
-                    <Button title="Already registered?" onPress={() => {
-                        props.navigation.navigate('Login');
-                    }}/>
-                    <Checkbox onSelect={onSelect} selected={selected}/>
-                </View>
-            </TouchableWithoutFeedback>
-        </Background>
+    props.navigation.navigate(
+      selected ? "TeacherHomeScreen" : "StudentHomeScreen"
     );
+  };
+
+  const onSelect = () => {
+    setSelected(!selected);
+  };
+
+  return (
+    <Background>
+      <TouchableWithoutFeedback
+        onPress={() => {
+          Keyboard.dismiss();
+        }}
+      >
+        <View style={styles.screen}>
+          <View style={styles.titleContainer}>
+            <AppTitle />
+          </View>
+          <View style={styles.inputFieldContainer}>
+            <TextInput
+              style={styles.inputField}
+              placeholder="Enter Email"
+              placeholderTextColor="white"
+              onChangeText={text => setEmail(text)}
+              value={email}
+            />
+          </View>
+          <View style={styles.inputFieldContainer}>
+            <TextInput
+              style={styles.inputField}
+              placeholder="Enter Full Name"
+              placeholderTextColor="white"
+              onChangeText={text => setName(text)}
+              value={email}
+            />
+          </View>
+          <View style={styles.inputFieldContainer}>
+            <TextInput
+              style={styles.inputField}
+              placeholder="Enter Course Code"
+              placeholderTextColor="white"
+              onChangeText={text => setCourseCode(text)}
+              value={email}
+            />
+          </View>
+          <View style={styles.inputFieldContainer}>
+            <TextInput
+              style={styles.inputField}
+              placeholder="Enter Password"
+              placeholderTextColor="white"
+              onChangeText={text => setPassword(text)}
+              value={password}
+            />
+          </View>
+          <StandardButton text="Register" onTap={onRegister} />
+          <Button
+            title="Already registered?"
+            onPress={() => {
+              props.navigation.navigate("Login");
+            }}
+          />
+          <Checkbox onSelect={onSelect} selected={selected} />
+        </View>
+      </TouchableWithoutFeedback>
+    </Background>
+  );
 };
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    titleContainer:{
-        paddingBottom: 5
-    },
-    inputFieldContainer: {
-        backgroundColor: 'rgba(0,0,0, 0.4)',
-        borderRadius: 20,
-        width: '75%',
-        height: 50,
-        flexDirection: "row",
-        alignItems: 'center',
-        justifyContent: 'space-around',
-        marginBottom: 10
-    },
-    inputField: {
-        color: 'white',
-        fontSize: 20
-    },
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  titleContainer: {
+    paddingBottom: 5
+  },
+  inputFieldContainer: {
+    backgroundColor: "rgba(0,0,0, 0.4)",
+    borderRadius: 20,
+    width: "75%",
+    height: 50,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    marginBottom: 10
+  },
+  inputField: {
+    color: "white",
+    fontSize: 20
+  }
 });
 
 export default RegisterScreen;
