303311cf55665e85b2d356e26ab53dfceefcb8fe
Francisco Romero <francisco.romero@sjsu.edu>
2020-01-28 13:54:09 -0800

    Added assignment list and conditional rendering

--- quickmaths/screens/ClassScreen.js
+++ quickmaths/screens/ClassScreen.js
@@ -1,20 +1,25 @@
 import React, { useState } from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import {useSelector, useDispatch} from 'react-redux';
+import {SwipeListView} from 'react-native-swipe-list-view';
 import {Ionicons} from '@expo/vector-icons';
 import {MaterialCommunityIcons} from '@expo/vector-icons';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 import {COURSES} from '../data/dummy-data';
 import Background from '../components/Background';
 import BackButton from '../constants/BackButton';
 import TabButton from '../components/TabButton';
+import ListItem from '../components/ListItem';
+import AddListItemButton from '../components/AddListItemButton';
 
 const ClassScreen = props => {
     const cId = props.navigation.getParam('classId');
-    const [isAssignmentsActive, setIsAssignmentsActive] = useState(false);
+    const [isAssignmentsActive, setIsAssignmentsActive] = useState(true);
     const [isSubmissionsActive, setIsSubmissionsActive] = useState(false);
     const [isRosterActive, setIsRosterActive] = useState(false);
     
-    const selectedClass = COURSES.find(c => c.id === cId);
+    const courseAssignments = useSelector(state => state.assignments.assignments);
 
     const onSelectAssignmentTab = () => {
         setIsAssignmentsActive(true);
@@ -35,6 +40,51 @@ const ClassScreen = props => {
         
     };
 
+    const renderListItem = (itemData) => {
+        return (
+            <ListItem 
+                topText={itemData.item.title} 
+                middleText={"Due " + itemData.item.dueDate}
+                bottomText={itemData.item.status + " " + itemData.item.currentDate}
+                bottomTextStyle={{fontStyle:"italic"}}
+                containerStyle={{width:'95%'}}
+                onSelect={() => {console.log("pressed!")}}
+            />
+        );
+    };
+
+    const RenderAssignments = () => {
+        return (
+        <SwipeListView 
+            keyExtractor={(item, index) => item.id}
+            data={courseAssignments} 
+            renderItem={renderListItem}
+            renderHiddenItem={(data, rowMap) => (
+            <View style={styles.backRow}>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity
+                        style={styles.backButton}
+                        onPress={() => {console.log("Delete")}}
+                    >
+                        <Ionicons name="ios-trash" size={75} color="white"/>
+                    </TouchableOpacity>
+                </View>
+            </View>
+                
+            )}
+            leftOpenValue={100}
+            ListFooterComponent= {
+                <AddListItemButton
+                    text='Create Assignment'
+                    containerStyle={{width:'95%'}}
+                    onSelect={() => {
+                        console.log('Added');
+                    }}
+                />
+            }
+        />
+    )};
+
     return(
         <Background>
             <View style={styles.screen}>
@@ -50,8 +100,11 @@ const ClassScreen = props => {
                         <Ionicons name="ios-people" size={30} color="white"/>
                     </TabButton>
                 </View>
-                <Text>The Class Screen!</Text>
-                <Text>{selectedClass.title}</Text>
+                {
+                    isAssignmentsActive ? <RenderAssignments/> :
+                    isSubmissionsActive ? null :
+                    null
+                }
             </View>
         </Background>
         
@@ -77,7 +130,24 @@ const styles = StyleSheet.create({
     tabContainer: {
         flexDirection: 'row',
         marginTop: 30,
-    }
+    },
+    backRow:{
+        flexDirection:'row',
+        alignItems:'center',
+        flex:1,
+    },
+    buttonContainer:{
+        backgroundColor: 'red',
+        width: 75,
+        left: 20,
+        justifyContent: 'center',
+        marginTop: 20,
+        borderRadius: 10
+    },
+    backButton:{
+        alignItems: 'center',
+        
+    },
 });
 
 export default ClassScreen;
