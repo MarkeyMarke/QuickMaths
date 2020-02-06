351e56eee0bcb971670df31818b3b47d227f568a
gregorymayo <gregoryjuevesii23@gmail.com>
2020-02-05 22:26:07 -0800

    user authentication Firebase finished

--- quickmaths/screens/LoginScreen.js
+++ quickmaths/screens/LoginScreen.js
@@ -1,83 +1,129 @@
-import React, { useState } from 'react';
-import {View, TextInput, Button, StyleSheet, Keyboard, TouchableWithoutFeedback} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, TextInput, Button, StyleSheet, Keyboard, TouchableWithoutFeedback, Alert } from 'react-native';
 
 import Background from '../components/Background';
 import AppTitle from '../constants/AppTitle';
 import StandardButton from '../components/StandardButton';
 
+import { useSelector, useDispatch } from 'react-redux';
+import * as usersAuthActions from '../store/actions/users';
+
 const LoginScreen = props => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+	const [ email, setEmail ] = useState('');
+	const [ password, setPassword ] = useState('');
+	const [ error, setError ] = useState();
+	const dispatch = useDispatch();
+	const isTeacher = useSelector(state => state.users.isTeacher);
+	//Function For An Allert
+	useEffect(
+		() => {
+			if (error) {
+				Alert.alert('An Error Occurred!', error, [ { text: 'Okay' } ]);
+			}
+		},
+		[ error ]
+	);
+	//Function For Login
+	const onLogin = async () => {
+		setError(null);
+		try {
+			await dispatch(usersAuthActions.signIn(email, password));
+			/*
+			const getUserIdentification = await fetch('https://quickmaths-bc73a.firebaseio.com/users.json');
+			const resData = await getUserIdentification.json();
+			var isTeacher = true;
+			for (const key in resData) {
+				if (resData[key].email == email) {
+					isTeacher = resData[key].teacher;
+				}
+            }
+            */
+			if (isTeacher)
+				//Teacher Home Page
+				props.navigation.navigate('TeacherHomeScreen');
+			else
+				//Student Home Page
+				props.navigation.navigate('StudentHomeScreen');
+		} catch (err) {
+			setError(err.message);
+		}
+	};
 
-    const onLogin = () => {
-        console.log(email);
-        console.log(password);
-        props.navigation.navigate('TeacherHomeScreen');
-    };
+	return (
+		<Background>
+			<TouchableWithoutFeedback
+				onPress={() => {
+					Keyboard.dismiss();
+				}}
+			>
+				<View style={styles.screen}>
+					<View style={styles.titleContainer}>
+						<AppTitle />
+					</View>
+					<View style={styles.inputFieldContainer}>
+						<TextInput
+							style={styles.inputField}
+							placeholder='Enter Email'
+							placeholderTextColor='white'
+							autoCapitalize='none'
+							onChangeText={text => setEmail(text)}
+							value={email}
+						/>
+					</View>
+					<View style={styles.inputFieldContainer}>
+						<TextInput
+							style={styles.inputField}
+							placeholder='Enter Password'
+							placeholderTextColor='white'
+							autoCapitalize='none'
+							secureTextEntry
+							onChangeText={text => setPassword(text)}
+							value={password}
+						/>
+					</View>
 
-    return(
-            <Background>
-                 <TouchableWithoutFeedback onPress={() => {Keyboard.dismiss()}}>
-                    <View style={styles.screen}>
-                        <View style={styles.titleContainer}>
-                            <AppTitle/>
-                        </View>
-                        <View style={styles.inputFieldContainer}>
-                            <TextInput
-                                style={styles.inputField}
-                                placeholder="Enter Email"
-                                placeholderTextColor='white'   
-                                onChangeText={(text) => setEmail(text)}
-                                value={email}
-                            />
-                        </View>
-                        <View style={styles.inputFieldContainer}>
-                            <TextInput
-                                style={styles.inputField}
-                                placeholder="Enter Password"
-                                placeholderTextColor='white'
-                                onChangeText={(text) => setPassword(text)}
-                                value={password}
-                            />
-                        </View>
-                        
-                        <StandardButton text="Login" onTap={onLogin}
-                        />
-                        <Button title="Forgot Password?" onPress={() => {
-                            props.navigation.navigate('EmailRecovery');
-                        }}/>
-                        <Button title="Not Registered?" onPress={() => {
-                            props.navigation.navigate('Register');
-                        }}/>
-                    </View>
-                </TouchableWithoutFeedback>
-            </Background>
-    );
+					<StandardButton text='Login' onTap={onLogin} />
+					<Button
+						title='Forgot Password?'
+						onPress={() => {
+							props.navigation.navigate('EmailRecovery');
+						}}
+					/>
+					<Button
+						title='Not Registered?'
+						onPress={() => {
+							props.navigation.navigate('Register');
+						}}
+					/>
+				</View>
+			</TouchableWithoutFeedback>
+		</Background>
+	);
 };
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    titleContainer:{
-        paddingBottom: 20
-    },
-    inputFieldContainer: {
-        backgroundColor: 'rgba(0,0,0, 0.4)',
-        borderRadius: 20,
-        width: '75%',
-        height: 50,
-        flexDirection: "row",
-        alignItems: 'center',
-        justifyContent: 'space-around',
-        marginTop: 10
-    },
-    inputField: {
-        color: 'white',
-        fontSize: 20
-    }
+	screen: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	titleContainer: {
+		paddingBottom: 20
+	},
+	inputFieldContainer: {
+		backgroundColor: 'rgba(0,0,0, 0.4)',
+		borderRadius: 20,
+		width: '75%',
+		height: 50,
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-around',
+		marginTop: 10
+	},
+	inputField: {
+		color: 'white',
+		fontSize: 20
+	}
 });
 
 export default LoginScreen;
