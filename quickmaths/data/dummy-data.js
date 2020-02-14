1587fe5c30316b994b30f75fb35a259d24303722
Francisco Romero <francisco.romero@sjsu.edu>
2020-02-14 15:30:10 -0800

    Created a new Class and updated Assignment parameters and updated the dummy data

--- quickmaths/data/dummy-data.js
+++ quickmaths/data/dummy-data.js
@@ -3,9 +3,9 @@ import Course from '../models/Courses';
 import Student from '../models/Student';
 
 export const COURSE_ASSIGNMENTS = [
-    new Assignment('a1', 'Long Division HW', '9/12/19', 'Published', '7/20/19', 30),
-    new Assignment('a2','Long Addition HW', '9/13/19', 'Published', '7/20/19', 23),
-    new Assignment('a3','Mixed Math HW', '9/25/19', 'Drafted', '7/29/19', 1),
+    new Assignment('a1', 'Long Division HW', '9/12/19', 'Published', '7/20/19', 30, []),
+    new Assignment('a2','Long Addition HW', '9/13/19', 'Published', '7/20/19', 23, []),
+    new Assignment('a3','Mixed Math HW', '9/25/19', 'Drafted', '7/29/19', 1, []),
 ];
 
 export const COURSES = [
@@ -15,9 +15,9 @@ export const COURSES = [
 ];
 
 export const STUDENT_ASSIGNMENTS = [
-    new Assignment('a1', 'Long Division HW', 'Due 9/12/19', '0/5 problems', '7/20/19', 30),
-    new Assignment('a2','Multiplication HW', 'Due 9/13/19', 'Completed', '7/20/19', 14),
-    new Assignment('a3','Long Subtraction HW', 'Due 9/10/19', '1/3 problems', '7/29/19', 12),
+    new Assignment('a1', 'Long Division HW', 'Due 9/12/19', '0/5 problems', '7/20/19', 30, []),
+    new Assignment('a2','Multiplication HW', 'Due 9/13/19', 'Completed', '7/20/19', 14, []),
+    new Assignment('a3','Long Subtraction HW', 'Due 9/10/19', '1/3 problems', '7/29/19', 12, []),
 ];
 
 export const STUDENTS = [
--- quickmaths/models/Assignment.js
+++ quickmaths/models/Assignment.js
@@ -1,11 +1,12 @@
 class Assignment {
-    constructor(id, title, dueDate, status, currentDate, submissions){
+    constructor(id, title, dueDate, status, currentDate, submissions, questions){
         this.id = id;
         this.title = title;
         this.dueDate = dueDate;
         this.status = status;
         this.currentDate = currentDate;
         this.submissions = submissions; //based on amount of students in class?
+        this.questions = questions;
     }
 }
 
