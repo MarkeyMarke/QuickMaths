9fb6820d6eacce71880354aaed3efa3dffe9009e
Francisco Romero <francisco.romero@sjsu.edu>
2020-02-18 12:56:02 -0800

    Created the editAssignment action and corresponding reducer

--- quickmaths/store/actions/assignments.js
+++ quickmaths/store/actions/assignments.js
@@ -9,3 +9,9 @@ export const DELETE_ASSIGNMENT = 'DELETE_ASSIGNMENT';
 export const deleteAssignment = (id) => {
     return { type: DELETE_ASSIGNMENT, assignmentId: id}
 };
+
+export const EDIT_ASSIGNMENT = 'EDIT_ASSIGNMENT';
+
+export const editAssignment = (id, name, date, questions) => {
+    return {type: EDIT_ASSIGNMENT, assignmentId: id, assignmentName: name, dueDate: date, questions: questions}
+};
