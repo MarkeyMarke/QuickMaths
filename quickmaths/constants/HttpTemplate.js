27cf0629f268ef41e3caef68593b072a11253c1c
Francisco Romero <francisco.romero@sjsu.edu>
2020-04-06 19:03:38 -0700

    Created file containing httpTemplate variable for http requests

---
+++ quickmaths/constants/HttpTemplate.js
@@ -0,0 +1,9 @@
+export const httpTemplate = {
+    method: "POST",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "*",
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    }
+};
